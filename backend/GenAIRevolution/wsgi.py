797402647f81be456315ca79281979770397fc6b
Tejas <tejasgulati11227@gmail.com>
2024-10-01 01:58:01 +0530

    Push

--- backend/GenAIRevolution/wsgi.py
+++ backend/GenAIRevolution/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GenAIRevolution.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.GenAIRevolution.settings')
 
 application = get_wsgi_application()
