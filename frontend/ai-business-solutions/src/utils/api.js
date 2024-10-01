afa43f28ef0d29fd472dc0ae58ea180c0bdc725c
Tejas <tejasgulati11227@gmail.com>
2024-10-01 11:11:49 +0530

    api.js

--- frontend/ai-business-solutions/src/utils/api.js
+++ frontend/ai-business-solutions/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',  // Update this to your Django backend URL
+  baseURL: 'https://ecopulse-app.onrender.com/',  // Update this to your Django backend URL
 });
 
 api.interceptors.response.use(
