64f9e4ae3df6169b0718a5c072f18d5c3bfad3ff
Tejas <tejasgulati11227@gmail.com>
2024-10-01 14:00:56 +0530

    Fixes

--- backend/GenAIRevolution/urls.py
+++ backend/GenAIRevolution/urls.py
@@ -1,23 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-from django.views.decorators.cache import never_cache
-
-# Load view
-index_view = never_cache(TemplateView.as_view(template_name='index.html'))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('ai_models.urls')),
     path('api/users/', include('users.urls')),
-    re_path(r'^$', index_view, name='index'),
-    re_path(r'^(?!api/)(?!admin/)(?!static/)(?!media/).*$', index_view),  # Match all non-api routes to the React app
+    re_path(r'^$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!api/)(?!admin/)(?!static/).+', TemplateView.as_view(template_name='index.html')),
 ]
 
-# Serve static files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# Serve media files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
